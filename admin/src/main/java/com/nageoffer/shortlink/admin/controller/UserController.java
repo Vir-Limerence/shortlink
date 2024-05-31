1aaee258247c6fbe2debe7e23d3e5da1cd0c1b99
maqi <2466570430@qq.com>
2024-05-31 16:44:51 +0800

    feature: 开发短链接后管系统相关功能

---
+++ admin/src/main/java/com/nageoffer/shortlink/admin/controller/UserController.java
@@ -0,0 +1,38 @@
+package com.nageoffer.shortlink.admin.controller;
+
+import com.nageoffer.shortlink.admin.common.convention.result.Result;
+import com.nageoffer.shortlink.admin.common.convention.result.Results;
+import com.nageoffer.shortlink.admin.dto.resp.UserRespDTO;
+import com.nageoffer.shortlink.admin.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/short-link/admin/v1/user")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    /**
+     * 根据用户名查找用户信息
+     */
+    @GetMapping("/{username}")
+    public Result<UserRespDTO> getUserByUsername(@PathVariable("username") String username){
+        return Results.success(userService.getUserByUserName(username));
+    }
+
+    /**
+     * 根据用户名查找用户无脱敏信息
+     */
+    @GetMapping("/actual/{username}")
+    public String getActualUserByUsername(@PathVariable("username") String username){
+        return "Hi " + username;
+    }
+
+
+
+}
